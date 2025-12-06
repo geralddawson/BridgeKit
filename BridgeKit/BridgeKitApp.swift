1d6a6be803a33c02cf8c643ca998f010627163e1
25GezzaDaw <59470932+25GezzaDaw@users.noreply.github.com>
2025-12-06 15:16:18 +1100

    Initial Commit

---
+++ BridgeKit/BridgeKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgeKitApp.swift
+//  BridgeKit
+//
+//  Created by Gerald Dawson on 6/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BridgeKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
